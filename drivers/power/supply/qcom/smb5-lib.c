d326eab0f3966405f4e5f78f6ec252012a89f80d
John Galt <johngaltfirstrun@gmail.com>
2022-07-22 09:48:57 -0400

    drivers/misc/power: implement passthrough hack interface
    Kanged from force fast charge toggle for laziness.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/power/supply/qcom/smb5-lib.c
+++ drivers/power/supply/qcom/smb5-lib.c
@@ -51,6 +51,11 @@
 #include "op_charge.h"
 #include <linux/oneplus/boot_mode.h>
 
+
+#ifdef CONFIG_OP_PASSTHROUGH_HACK
+#include <linux/passthroughhack.h>
+#endif
+
 #define SOC_INVALID                   0x7E
 #define SOC_DATA_REG_0                0x88D
 #define SOC_FLAG_REG                  0x88E
@@ -8092,6 +8097,9 @@ static void set_usb_switch(struct smb_charger *chg, bool enable)
 		pr_debug("no fast_charger register found\n");
 		return;
 	}
+
+	if (passthrough_hack > 0)
+		return;
 
 	if (enable) {
 		pr_debug("switch on fastchg\n");
